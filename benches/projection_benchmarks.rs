1859cea955d47ffb31a66762dae43300187ebd00
kinkard <stsiapan.kizim@gmail.com>
2025-07-31 23:18:07 +0200

    feat: `distance_to_segment() and `square_distance_to_segment`

--- benches/projection_benchmarks.rs
+++ benches/projection_benchmarks.rs
@@ -2,15 +2,15 @@ use criterion::{Criterion, criterion_group, criterion_main};
 use plane_projection::PlaneProjection;
 use std::hint::black_box;
 
-fn bench_plane_projection_distance(c: &mut Criterion) {
-    c.bench_function("plane_projection_distance", |b| {
+fn bench_distance(c: &mut Criterion) {
+    c.bench_function("distance", |b| {
         b.iter(|| {
             let projection = PlaneProjection::new(black_box(55.65));
             black_box(projection.distance(black_box((55.60, 13.5)), black_box((55.61, 13.53))))
         });
     });
 
-    c.bench_function("reused_plane_projection_distance", |b| {
+    c.bench_function("reused projection distance", |b| {
         let projection = PlaneProjection::new(55.65);
         b.iter(|| {
             black_box(projection.distance(black_box((55.60, 13.5)), black_box((55.61, 13.53))))
@@ -18,8 +18,53 @@ fn bench_plane_projection_distance(c: &mut Criterion) {
     });
 }
 
-fn bench_plane_projection_heading(c: &mut Criterion) {
-    c.bench_function("plane_projection_heading", |b| {
+fn bench_distance_to_segment(c: &mut Criterion) {
+    // Points in circle to check all branches for the "distance to segment" function
+    let base = (55.60, 13.5);
+    let segment = ((base.0, base.0 - 0.5), (base.0, base.0 + 0.5));
+    let points = (0..100)
+        .map(|i| {
+            let angle = 2.0 * std::f64::consts::PI * i as f64 / 100.0;
+            (base.0 + angle.cos(), base.1 + angle.sin())
+        })
+        .collect::<Vec<(f64, f64)>>();
+
+    c.bench_function("distance to segment", |b| {
+        b.iter(|| {
+            for point in &points {
+                let projection = PlaneProjection::new(black_box(base.0));
+                black_box(projection.distance_to_segment(black_box(*point), black_box(segment)));
+            }
+            // add degenerate cases
+            let projection = PlaneProjection::new(black_box(base.0));
+            black_box(
+                projection.distance_to_segment(black_box(base), black_box((segment.0, segment.0))),
+            );
+            let projection = PlaneProjection::new(black_box(base.0));
+            black_box(
+                projection.distance_to_segment(black_box(base), black_box((segment.1, segment.1))),
+            );
+        });
+    });
+
+    c.bench_function("reused projection distance to segment", |b| {
+        let projection = PlaneProjection::new(black_box(base.0));
+        b.iter(|| {
+            for point in &points {
+                black_box(projection.distance_to_segment(black_box(*point), black_box(segment)));
+            }
+            black_box(
+                projection.distance_to_segment(black_box(base), black_box((segment.0, segment.0))),
+            );
+            black_box(
+                projection.distance_to_segment(black_box(base), black_box((segment.1, segment.1))),
+            );
+        });
+    });
+}
+
+fn bench_heading(c: &mut Criterion) {
+    c.bench_function("heading", |b| {
         b.iter(|| {
             let projection = PlaneProjection::new(black_box(55.65));
             black_box(projection.heading(
@@ -29,7 +74,7 @@ fn bench_plane_projection_heading(c: &mut Criterion) {
         });
     });
 
-    c.bench_function("reused_plane_projection_heading", |b| {
+    c.bench_function("reused projection heading", |b| {
         let projection = PlaneProjection::new(55.65);
         b.iter(|| {
             black_box(projection.heading(
@@ -42,7 +87,8 @@ fn bench_plane_projection_heading(c: &mut Criterion) {
 
 criterion_group!(
     benches,
-    bench_plane_projection_distance,
-    bench_plane_projection_heading,
+    bench_distance,
+    bench_distance_to_segment,
+    bench_heading,
 );
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ const EQUATORIAL_RADIUS: f64 = 6378137.0;
 const FLATTENING: f64 = 1.0 / 298.257223563;
 const SQUARED_ECCENTRICITY: f64 = FLATTENING * (2.0 - FLATTENING);
 
-/// A coordinate in (latitude, longitude) format
+/// A coordinate in (latitude, longitude) format.
 pub type LatLon = (f64, f64);
 
 /// A plane projection, useful for blazingly fast approximate distance calculations.
@@ -28,7 +28,7 @@ pub struct PlaneProjection {
 }
 
 impl PlaneProjection {
-    /// Creates a plane projection to the Earth at provided latitude
+    /// Creates a plane projection to the Earth at provided latitude.
     pub fn new(latitude: f64) -> Self {
         // Based on https://en.wikipedia.org/wiki/Earth_radius#Meridional
         let cos_lat = latitude.to_radians().cos();
@@ -45,7 +45,7 @@ impl PlaneProjection {
         }
     }
 
-    /// Sqare distance in meters between two points in (lat, lon) format
+    /// Square distance in meters between two points in (lat, lon) format.
     #[inline(always)]
     pub fn square_distance(&self, a: LatLon, b: LatLon) -> f64 {
         let lat_dist = (a.0 - b.0) * self.lat_scale;
@@ -53,10 +53,41 @@ impl PlaneProjection {
         lat_dist * lat_dist + lon_dist * lon_dist
     }
 
-    /// Distance in meters between two points in (lat, lon) format
+    /// Distance in meters between two points in (lat, lon) format.
     #[inline(always)]
     pub fn distance(&self, a: LatLon, b: LatLon) -> f64 {
         self.square_distance(a, b).sqrt()
+    }
+
+    /// Square distance in meters from point to the segment.
+    pub fn square_distance_to_segment(&self, point: LatLon, segment: (LatLon, LatLon)) -> f64 {
+        // Convert point and segment to projected space with origin at segment start
+        let mut point = (
+            (point.0 - segment.0.0) * self.lat_scale,
+            lon_diff(point.1, segment.0.1) * self.lon_scale,
+        );
+        let segment = (
+            (segment.1.0 - segment.0.0) * self.lat_scale,
+            lon_diff(segment.1.1, segment.0.1) * self.lon_scale,
+        );
+        if segment.0 != 0.0 || segment.1 != 0.0 {
+            let projection = (point.0 * segment.0 + point.1 * segment.1)
+                / (segment.0 * segment.0 + segment.1 * segment.1);
+            if projection > 1.0 {
+                point.0 -= segment.0;
+                point.1 -= segment.1;
+            } else if projection > 0.0 {
+                point.0 -= segment.0 * projection;
+                point.1 -= segment.1 * projection;
+            }
+        }
+        point.0 * point.0 + point.1 * point.1
+    }
+
+    /// Distance in meters from point to the segment.
+    #[inline(always)]
+    pub fn distance_to_segment(&self, point: LatLon, segment: (LatLon, LatLon)) -> f64 {
+        self.square_distance_to_segment(point, segment).sqrt()
     }
 
     /// Heading (azimuth) in degrees from point `a` to point `b` in the range [0.0, 360.0) degrees,
@@ -72,7 +103,7 @@ impl PlaneProjection {
     }
 }
 
-/// Returns the difference between two longitudes in range [-180.0, 180.0] degrees
+/// Returns the difference between two longitudes in range [-180.0, 180.0] degrees.
 #[inline(always)]
 fn lon_diff(a: f64, b: f64) -> f64 {
     let mut lon_diff = a - b;
@@ -87,6 +118,9 @@ fn lon_diff(a: f64, b: f64) -> f64 {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    const MALMO_C: LatLon = (55.60330902847681, 13.001973666557435);
+    const LUND_C: LatLon = (55.704141722528554, 13.191304107330561);
 
     #[test]
     fn lon_diff_test() {
@@ -105,20 +139,58 @@ mod tests {
 
     #[test]
     fn distance_test() {
-        // From Lund C to Malmo C
         let proj = PlaneProjection::new(55.65);
-        assert_eq!(
-            proj.distance(
-                (55.704141722528554, 13.191304107330561),
-                (55.60330902847681, 13.001973666557435),
-            ) as u32,
-            16373
-        );
+        assert_eq!(proj.distance(MALMO_C, LUND_C,) as u32, 16373);
 
         let proj = PlaneProjection::new(51.05);
         assert_eq!(
             proj.distance((50.823194, 6.186389), (51.301389, 6.953333)) as u32,
             75646
+        );
+    }
+
+    #[test]
+    fn distance_to_segment_test() {
+        let proj = PlaneProjection::new(0.0);
+        assert_eq!(
+            proj.distance_to_segment((0.0, 0.0), ((0.0, 0.0), (0.0, 1.0))),
+            0.0
+        );
+        assert_eq!(
+            proj.distance_to_segment((0.0, 0.5), ((0.0, 0.0), (0.0, 1.0))),
+            0.0
+        );
+        assert_eq!(
+            proj.distance_to_segment((0.0, 1.0), ((0.0, 0.0), (0.0, 1.0))),
+            0.0
+        );
+        assert_eq!(
+            proj.distance_to_segment((0.0, 0.0), ((0.0, 1.0), (0.0, 2.0))),
+            proj.distance((0.0, 0.0), (0.0, 1.0))
+        );
+        assert_eq!(
+            proj.distance_to_segment((0.0, 4.5), ((0.0, 1.0), (0.0, 3.0))),
+            proj.distance((0.0, 4.5), (0.0, 3.0))
+        );
+
+        // zero-length segment
+        assert_eq!(
+            proj.distance_to_segment((1.0, 2.0), ((3.0, 3.0), (3.0, 3.0))),
+            proj.distance((1.0, 2.0), (3.0, 3.0))
+        );
+
+        let proj = PlaneProjection::new(55.65);
+        assert_eq!(
+            proj.distance_to_segment(MALMO_C, (LUND_C, LUND_C)) as u32,
+            16373
+        );
+
+        // This is why it's tricky - point get projected not to a straight line in the lat/lon space,
+        // but to a segment in the projected space.
+        assert_eq!(
+            proj.distance_to_segment((55.67817981392954, 13.058789566271836), (MALMO_C, LUND_C))
+                as u32,
+            3615
         );
     }
 
@@ -130,20 +202,7 @@ mod tests {
         assert_eq!(proj.heading((55.70, 13.19), (55.70, 13.29)) as i32, 90);
         assert_eq!(proj.heading((55.70, 13.19), (55.70, 13.09)) as i32, 270);
 
-        // From Malmo C to Lund C
-        assert_eq!(
-            proj.heading(
-                (55.60330902847681, 13.001973666557435),
-                (55.704141722528554, 13.191304107330561),
-            ) as i32,
-            46
-        );
-        assert_eq!(
-            proj.heading(
-                (55.704141722528554, 13.191304107330561),
-                (55.60330902847681, 13.001973666557435),
-            ) as i32,
-            180 + 46
-        );
+        assert_eq!(proj.heading(MALMO_C, LUND_C,) as i32, 46);
+        assert_eq!(proj.heading(LUND_C, MALMO_C,) as i32, 180 + 46);
     }
 }
